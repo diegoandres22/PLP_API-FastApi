3562d2fa2f84234cb891371ad837298544409376
diegoandres22 <diego.a.v3005@gmail.com>
2025-09-02 23:41:03 -0400

    FEAT: mejora la gestión de CORS y seguridad en la aplicación, permitiendo configuraciones dinámicas desde variables de entorno

--- src/services/gcs_service.py
+++ src/services/gcs_service.py
@@ -33,22 +33,3 @@ def upload_file_to_gcs(file_bytes: bytes, filename: str) -> str:
         raise RuntimeError("No se pudo subir la imagen. Revisa la configuración del bucket y las credenciales.")
 
 
-
-# from google.cloud import storage
-# import uuid
-
-# BUCKET_NAME = "patelalaperola_imagenes"
-
-# def upload_file_to_gcs(file_bytes: bytes, filename: str) -> str:
-#     client = storage.Client()
-#     bucket = client.bucket(BUCKET_NAME)
-
-#     unique_filename = f"{uuid.uuid4()}_{filename}"
-
-#     blob = bucket.blob(unique_filename)
-#     blob.upload_from_string(file_bytes)
-
-#     # NO usar blob.make_public()
-
-#     # Retorna URL pública (asumiendo permisos públicos en bucket)
-#     return f"https://storage.googleapis.com/{BUCKET_NAME}/{unique_filename}"
