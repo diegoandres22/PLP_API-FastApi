4bd928cada9cd0122987416aaf6ed11d491e1c94
Andres D. Moya <89799715+diegoandres22@users.noreply.github.com>
2026-05-21 18:34:47 -0400

    FIX: add "pool_pre_ping=True" and "sslmode=require"

--- src/db/db.py
+++ src/db/db.py
@@ -23,7 +23,12 @@ load_dotenv()
 # )
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}
+)
+# engine = create_engine(DATABASE_URL)
 # engine = create_engine(url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
